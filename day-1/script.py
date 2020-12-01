e9ff49900aa21d9273aabce46b43927d64642da6
elen-oui <elenipissaridou@gmail.com>
2020-12-01 21:22:09 +0000

    Rename diff to expected

--- day-1/script.py
+++ day-1/script.py
@@ -1,7 +1,9 @@
 input_file = open('input.txt', 'r')
 input_lines = input_file.readlines()
 input_numbers = {int(i) for i in input_lines}
+
 for i in input_numbers:
-    diff = 2020-i
-    if diff in input_numbers:
-        print(i * diff)
+    # what do I add to i to get 2020?
+    expected = 2020-i
+    if expected in input_numbers:
+        print(i * expected)
