3169e5e8ed454b27549f9b9a997568e16ae5a444
elen-oui <elenipissaridou@gmail.com>
2020-12-01 22:05:49 +0000

    Add part B

--- day-1/script.py
+++ day-1/script.py
@@ -2,8 +2,18 @@ input_file = open('input.txt', 'r')
 input_lines = input_file.readlines()
 input_numbers = {int(i) for i in input_lines}
 
+# Part A
+
 for i in input_numbers:
     # what do I add to i to get 2020?
     expected = 2020-i
     if expected in input_numbers:
         print(i * expected)
+
+# Part B
+        
+for i in input_numbers:
+    for j in input_numbers:
+        expected = 2020-i-j
+        if expected in input_numbers:
+            print(i * j * expected)
